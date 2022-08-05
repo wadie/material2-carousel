da1504a5e148ac42f849030cb7621b738c60ca07
Wadie <wadie.farhat@verso.de>
2022-08-05 11:29:35 +0200

    angular 14.1.1

--- projects/carousel/src/lib/carousel.module.ts
+++ projects/carousel/src/lib/carousel.module.ts
@@ -1,15 +1,13 @@
-import { NgModule, Injectable, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Injectable, ModuleWithProviders, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-import { MatCarouselComponent } from './carousel.component';
-import { MatCarouselSlideComponent } from './carousel-slide/carousel-slide.component';
 import {
-  HammerGestureConfig,
-  HAMMER_GESTURE_CONFIG,
-  HammerModule
+  HammerGestureConfig, HammerModule, HAMMER_GESTURE_CONFIG
 } from '@angular/platform-browser';
+import { MatCarouselSlideComponent } from './carousel-slide/carousel-slide.component';
+import { MatCarouselComponent } from './carousel.component';
 
 // https://github.com/angular/angular/issues/10541#issuecomment-300761387
 @Injectable()
