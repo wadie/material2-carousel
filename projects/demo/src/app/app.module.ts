16caca5f31e089307d077eacbcd29705b7369690
Wadie <wadie.farhat@verso.de>
2022-08-10 14:26:06 +0200

    add angular.json

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -13,9 +13,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatCarouselModule } from 'material2-carousel';
 
 import { AppComponent } from './app.component';
+import { MatCarouselModule } from 'material2-carousel';
 
 @NgModule({
   declarations: [AppComponent],
